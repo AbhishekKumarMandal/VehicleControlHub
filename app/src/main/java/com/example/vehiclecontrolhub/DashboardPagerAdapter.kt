955043b13425972a8b93e9fc3575993166d33bc1
AbhishekKumarMandal <abhishekmandal026@gmail.com>
2026-05-17 22:41:55 +0530

    added LightFragment object in Page adapter

--- app/src/main/java/com/example/vehiclecontrolhub/DashboardPagerAdapter.kt
+++ app/src/main/java/com/example/vehiclecontrolhub/DashboardPagerAdapter.kt
@@ -22,6 +22,7 @@ class DashboardPagerAdapter(activity: AppCompatActivity) : FragmentStateAdapter(
             0 -> TelemetryFragment()
             1 -> HvacFragment()
             2 -> SeatFragment() //PlaceholderFragment.newInstance("Seats - Coming soon")
+            3 -> LightFragment() //PlaceholderFragment.newInstance("Lights - Coming soon")
             4 -> BodyFragment() //PlaceholderFragment.newInstance("Body Control - Coming soon")
             5 -> CameraFragment()
             else -> PlaceholderFragment.newInstance("Coming soon")
